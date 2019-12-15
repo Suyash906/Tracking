6cc5d09342a0955d3d9f7ed9fa67fc5e9a5eb6e9
wamiquem <wamique.ansari@sjsu.edu>
2019-12-14 19:58:07 -0800

    Changed code to reflect storage grpc functions

--- grpc_client.py
+++ grpc_client.py
@@ -1,6 +1,6 @@
 import grpc
-import upload_pb2
-import upload_pb2_grpc
+import storage_pb2
+import storage_pb2_grpc
 import threading
 import io
 import hashlib
@@ -12,12 +12,12 @@ def get_file_byte_chunks(f):
         piece = f.read(CHUNK_SIZE)
         if len(piece) == 0:
             return
-        yield upload_pb2.Chunk(content=piece)
+        yield storage_pb2.ChunkRequest(chunk=piece)
 
 class Client:
     def __init__(self, address):
             channel = grpc.insecure_channel(address)
-            self.stub = upload_pb2_grpc.FileServerStub(channel)
+            self.stub = storage_pb2_grpc.FileServerStub(channel)
     
     def upload(self, f, f_name):
         print("Inside here")
@@ -25,5 +25,15 @@ class Client:
         hex_dig = hash_object.hexdigest()
         print(hex_dig)
         chunks_generator = get_file_byte_chunks(f)
-        response = self.stub.upload(chunks_generator)
+        metadata = (
+            ('key-hash-id', hex_dig),
+            ('key-chunk-size', str(CHUNK_SIZE))
+        )
+        response = self.stub.upload_chunk_stream(chunks_generator, metadata=metadata)
         
+    def download(self, f_name):
+        hash_object = hashlib.sha1(f_name.encode())
+        hex_dig = hash_object.hexdigest()
+        response = self.stub.download_chunk_stream(chunk_pb2.Request(hash_id=hex_dig))
+        # print("Successfully downloaded file with hash_id: ", hash_id)
+        # return response
