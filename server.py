92cb534c40fbb04865caa104f495c3e5c4ef9feb
wamiquem <wamique.ansari@sjsu.edu>
2019-12-16 15:34:42 -0800

    read logic update

--- server.py
+++ server.py
@@ -3,7 +3,9 @@ import logging, os
 from werkzeug.utils import secure_filename
 from flask import jsonify, make_response
 import roundrobin
-import grpc_client
+import storage_client
+import time
+import urllib
 
 app = Flask(__name__)
 file_handler = logging.FileHandler('server.log')
@@ -25,12 +27,16 @@ def create_new_folder(local_dir):
 def api_root():
     app.logger.info(PROJECT_HOME)
     if request.method == 'POST' and request.files['image']:
+        start = time.time()
         app.logger.info(app.config['UPLOAD_FOLDER'])
         img = request.files['image']
         img_name = secure_filename(img.filename)
-        # client = grpc_client.Client(roundrobin.getIpAddress())
-        client = grpc_client.Client("127.0.0.1:2750")
+        # client = storage_client.Client(roundrobin.getIpAddress())
+        client = storage_client.Client("127.0.0.1:2750")
         client.upload(img, img_name)
+        end = time.time()
+        print("File size in bytes==", 119669)
+        print("Total time with file im memory for upload= ",end - start)
         return make_response(jsonify({"success":True}),200)
     else:
         return "Where is the file?"
@@ -44,10 +50,15 @@ def getFile():
             return make_response(jsonify({"success":False,"error":"Content is Missing"}),501)
         else:
             # Get the file from grpc using file ID from request
+            fileId = urllib.parse.unquote(fileId)
             
             hashedFileId = secure_filename(fileId)
-            client = grpc_client.Client("127.0.0.1:2750")
+            print("FILE ID  :: ",fileId,hashedFileId);
+            client = storage_client.Client("127.0.0.1:2750")
             result = client.download(hashedFileId)
+
+            print("===============result==============")
+            print(result)
 
             #return file in bytes array to client in "content"
             return make_response(jsonify({"success":True, "content": result}),200)
@@ -62,15 +73,20 @@ def addFile():
             data = request.get_json()
             fileId = data['fileId']
             file = data['content']
+            fileSize = data['size']
 
             if fileId is None or file is None:
                 print("NO CONTENT")
                 return make_response(jsonify({"success":False,"error":"Content is Missing"}),501)
             else:
-                hashedFileId = secure_filename(fileId)
+                # hashedFileId = secure_filename(fileId)
                 # Send This to GRPC function
-                client = grpc_client.Client(roundrobin.getIpAddress())
-                client.upload(file, hashedFileId)
+                # client = storage_client.Client(roundrobin.getIpAddress())
+                # client.upload(file, hashedFileId)
+                hashedFileId = secure_filename(fileId)
+                print("FILE ID  :: ",fileId,hashedFileId);
+                client = storage_client.Client("127.0.0.1:2750")
+                success = client.upload(file, hashedFileId, fileSize)
 
                 return make_response(jsonify({"success":True}),200)
         except Exception as e:
@@ -89,8 +105,8 @@ def getMessage():
         if messageId is None :
             return make_response(jsonify({"success":False,"error":"Content is Missing"}),501)
         else:
-            # client = grpc_client.Client(roundrobin.getIpAddress())
-            client = grpc_client.Client("127.0.0.1:2750")
+            # client = storage_client.Client(roundrobin.getIpAddress())
+            client = storage_client.Client("127.0.0.1:2750")
             result = client.getMessage(messageId)
             return make_response(jsonify({"success":True, "message": result, "messageId": messageId}),200)
     else:
@@ -107,8 +123,8 @@ def addMessage():
             if messageId is None or message is None:
                 return make_response(jsonify({"success":False,"error":"Content is Missing !"}),501)
             else:
-                # client = grpc_client.Client(roundrobin.getIpAddress())
-                client = grpc_client.Client("127.0.0.1:2750")
+                # client = storage_client.Client(roundrobin.getIpAddress())
+                client = storage_client.Client("127.0.0.1:2750")
                 print("message==", message)
                 print("messageId==", messageId)
                 client.sendMessage(message, messageId)
--- storage_client.py
+++ storage_client.py
@@ -1,44 +1,62 @@
 import grpc
 import storage_pb2
 import storage_pb2_grpc
-import threading
 import io
 import hashlib
 import math
 import sys
+import os
+import funcy
 
-CHUNK_SIZE = 1024 * 1024 * 3  # 4MB
+CHUNK_SIZE = 1024 * 1024 * 3  # 3MB
 NO_OF_CHUNKS = 0
 
-def get_file_byte_chunks(f):
-    while True:
-        piece = f.read(CHUNK_SIZE)
-        if len(piece) == 0:
-            return
-        yield storage_pb2.ChunkRequest(chunk=piece)
+def get_file_byte_chunks(f, size_of_chunk):
+    # while True:
+    #     piece = f.read(CHUNK_SIZE)
+    #     if len(piece) == 0:
+    #         return
+    #     yield storage_pb2.ChunkRequest(chunk=piece)
+    # print("size_of_chunk ==",size_of_chunk)
+    # print("no_of_chunks ==",no_of_chunks)
+    # for i in range(no_of_chunks):
+    #     print("(size_of_chunk*i)==", (size_of_chunk*i))
+    #     print("(i+1)*size_of_chunk-1==", (i+1)*size_of_chunk-1)
+    #     print("f[(size_of_chunk*i):(i+1)*size_of_chunk-1]=====", f[(size_of_chunk*i):(i+1)*size_of_chunk-1])
+    #     yield storage_pb2.ChunkRequest(chunk=f[(size_of_chunk*i):(i+1)*size_of_chunk-1])
+    chunk_list = list(funcy.chunks(size_of_chunk, f))
+
+    for chunk in chunk_list:
+        print("chunk==",chunk)
+        yield storage_pb2.ChunkRequest(chunk=chunk)
 
 class Client:
     def __init__(self, address):
             channel = grpc.insecure_channel(address)
             self.stub = storage_pb2_grpc.FileServerStub(channel)
     
-    def upload(self, f, f_name):
+    def upload(self, f, f_name, size):
         print("Inside here")
         hash_object = hashlib.sha1(f_name.encode())
         hex_dig = hash_object.hexdigest()
         print(hex_dig)
 
-        # data_size = len(f.read())
-        data_size = 44740
-        print("data_size==", data_size)
-        no_of_chunks = math.ceil(data_size / CHUNK_SIZE)
-        chunks_generator = get_file_byte_chunks(f)
+        #data_size = len(f.read())
+        # data_size = os.path.getsize("/Users/wamiqueansari/Downloads/tweet-unlike-icon.png")
+        # data_size = 9658
+        fBytes = str.encode(f)
+        print("data_size==", size)
+        no_of_chunks = math.ceil(size / CHUNK_SIZE)
+        size_of_chunk = math.ceil(size / no_of_chunks)
+        chunks_generator = get_file_byte_chunks(fBytes, size_of_chunk)
+        print("testtttttttt")
         metadata = (
             ('key-hash-id', hex_dig),
             ('key-chunk-size', str(CHUNK_SIZE)),
             ('key-number-of-chunks', str(no_of_chunks))
         )
         response = self.stub.upload_chunk_stream(chunks_generator, metadata=metadata)
+        
     
     def sendMessage(self, message, messageId):
         print("Inside Client Method - send")
@@ -64,9 +82,16 @@ class Client:
         hash_object = hashlib.sha1(f_name.encode())
         hex_dig = hash_object.hexdigest()
         response = self.stub.download_chunk_stream(storage_pb2.HashIdRequest(hash_id=hex_dig))
-        with open("./"+f_name,'wb') as f:
-            for c in response:
-                f.write(c.chunk)
+        print('==============response==============')
+        print(response)
+        # with open("./"+f_name,'wb') as f:
+        #     for c in response:
+        #         f.write(c.chunk)
+        file_bytes = bytearray()
+        for c in response:
+            file_bytes.extend(c.chunk)
+        file = file_bytes.decode()
+        return file
     
     def getMessage(self, messageId):
         hash_object = hashlib.sha1(messageId.encode())
