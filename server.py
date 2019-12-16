e8e18d4275bb2575840e7f409592eec0553b5820
wamiquem <wamique.ansari@sjsu.edu>
2019-12-15 22:16:24 -0800

    Storage Integration

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, send_from_directory, request
 import logging, os
 from werkzeug.utils import secure_filename
 from flask import jsonify, make_response
-# import worker
+import roundrobin
 import grpc_client
 
 app = Flask(__name__)
@@ -28,81 +28,15 @@ def api_root():
         app.logger.info(app.config['UPLOAD_FOLDER'])
         img = request.files['image']
         img_name = secure_filename(img.filename)
-        client = grpc_client.Client('localhost:9999')
+        # client = grpc_client.Client(roundrobin.getIpAddress())
+        client = grpc_client.Client("127.0.0.1:2750")
         client.upload(img, img_name)
-        # create_new_folder(app.config['UPLOAD_FOLDER'])
-        # saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
-        # app.logger.info("saving {}".format(saved_path))
-        # img.save(saved_path)
-        # worker.enqueue_request_queue(img.filename,"write")
-        # worker.blockThread()
-        # return make_response(jsonify({"success":True,"filename":img.filename}),200)
         return make_response(jsonify({"success":True}),200)
     else:
         return "Where is the file?"
 
-
-@app.route('/addMessage', methods = ['POST'])
-def addMessage():
-    if request.method == 'POST':
-        data = request.get_json()
-        messageId = data['messageId']
-        message = data['message']
-        if messageId is None or message is None:
-            return make_response(jsonify({"success":False,"error":"Content is Missing"}),501)
-        else:
-            messageBytes = bytearray(message)
-            # Send This to GRPC function
-
-            return make_response(jsonify({"success":True}),200)
-    else:
-        return make_response(jsonify({"success":False,"error":"No Message Present"}),501)
-
-
-@app.route('/addFile', methods = ['POST'])
-def addFile():
-    if request.method == 'POST':
-        data = request.get_json()
-        fileId = data['fileId']
-        file = data['content']
-
-        if fileId is None or file is None:
-            return make_response(jsonify({"success":False,"error":"Content is Missing"}),501)
-        else:
-            hashedFileId = secure_filename(fileId)
-            # Send This to GRPC function
-            client = grpc_client.Client('localhost:9999')
-            client.upload(fileId, hashedFileId)
-
-            # variables2 = json.loads(s) # to convert back into dictionary
-            return make_response(jsonify({"success":True}),200)
-    else:
-        return make_response(jsonify({"success":False,"error":"No File Present"}),501)
-
-
-@app.route('/getMessage', methods = ['GET'])
-def getMessage(messageId):
-    if request.method == 'GET':
-        messageId = request.args.get('messageId')
-
-        if messageId is None :
-            return make_response(jsonify({"success":False,"error":"Content is Missing"}),501)
-        else:
-            #Get the message from grpc using message ID from request
-
-            client = grpc_client.Client('localhost:9999')
-            # Send messageId to GRPC function and get message in response
-            messageBytes = client.upload(messageId)
-            # convert message byte array to message and return to user
-            message = bytearray(messageBytes)
-            #return message in bytes array to client in "content"
-            return make_response(jsonify({"success":True, "message": message, "messageId": messageId}),200)
-    else:
-        return make_response(jsonify({"success":False,"error":"No Message Present"}),501)
-
-
 @app.route('/getFile', methods = ['GET'])
-def getFile(fileId):
+def getFile():
     if request.method == 'GET':
         fileId = request.args.get('fileId')
 
@@ -111,16 +45,80 @@ def getFile(fileId):
         else:
             # Get the file from grpc using file ID from request
             
-            # fileBytes = bytearray(file)
-            # # fileBytes = json.dumps(myObject)# dictionary to bytes
-            # # Send This to GRPC function
-            # # variables2 = json.loads(s) # to convert back into dictionary
+            hashedFileId = secure_filename(fileId)
+            client = grpc_client.Client("127.0.0.1:2750")
+            result = client.download(hashedFileId)
 
             #return file in bytes array to client in "content"
-            return make_response(jsonify({"success":True, "content": "bytearray"}),200)
+            return make_response(jsonify({"success":True, "content": result}),200)
     else:
         return make_response(jsonify({"success":False,"error":"No File Present"}),501)
 
+
+@app.route('/addFile', methods = ['POST'])
+def addFile():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            fileId = data['fileId']
+            file = data['content']
+
+            if fileId is None or file is None:
+                print("NO CONTENT")
+                return make_response(jsonify({"success":False,"error":"Content is Missing"}),501)
+            else:
+                hashedFileId = secure_filename(fileId)
+                # Send This to GRPC function
+                client = grpc_client.Client(roundrobin.getIpAddress())
+                client.upload(file, hashedFileId)
+
+                return make_response(jsonify({"success":True}),200)
+        except Exception as e:
+            print("EXCEPTION",e)
+            return make_response(jsonify({"success":False,"error":"Content is Missing"}),501)
+    else:
+        print("NO POST")
+        return make_response(jsonify({"success":False,"error":"No File Present"}),501)
+
+
+@app.route('/getMessage', methods = ['GET'])
+def getMessage():
+    if request.method == 'GET':
+        messageId = request.args.get('messageId')
+        print("messageId--get--==", messageId)
+        if messageId is None :
+            return make_response(jsonify({"success":False,"error":"Content is Missing"}),501)
+        else:
+            # client = grpc_client.Client(roundrobin.getIpAddress())
+            client = grpc_client.Client("127.0.0.1:2750")
+            result = client.getMessage(messageId)
+            return make_response(jsonify({"success":True, "message": result, "messageId": messageId}),200)
+    else:
+        return make_response(jsonify({"success":False,"error":"No Message Present"}),501)
+
+@app.route('/addMessage', methods = ['POST'])
+def addMessage():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            messageId = data['messageId']
+            message = data['message']
+            
+            if messageId is None or message is None:
+                return make_response(jsonify({"success":False,"error":"Content is Missing !"}),501)
+            else:
+                # client = grpc_client.Client(roundrobin.getIpAddress())
+                client = grpc_client.Client("127.0.0.1:2750")
+                print("message==", message)
+                print("messageId==", messageId)
+                client.sendMessage(message, messageId)
+                return make_response(jsonify({"success":True}),200)
+        except Exception as e:
+            print("EXCEPTION",e)
+            return make_response(jsonify({"success":False,"error":"Content is Missing"}),501)
+    else:
+        print("NO POST")
+        return make_response(jsonify({"success":False,"error":"No Message Present"}),501)
 
 
 if __name__ == '__main__':
